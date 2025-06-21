0ef03d14d17f33277a6091ce10e4a3978bf7c292
Ilaha <ilaharf-bp218@code.edu.az>
2025-06-21 20:48:57 +0400

    Add register logic,test auth endpoints,fix auth errors

--- BE/src/controllers/authController.js
+++ BE/src/controllers/authController.js
@@ -5,15 +5,15 @@ import jwt from "jsonwebtoken"
 
 export const loginController = async (req, res) => {
     try {
-        const { username, password, email } = req.body
-        const user = await userModel.findOne({username:username})
+        const { password, email } = req.body
+        const user = await userModel.findOne({ email })
         if (!user) {
             return res.send("User not found")
         }
-        if (user.password!==password) {
-           return res.send("Wrong password") 
+        if (user.password !== password) {
+            return res.send("Wrong password")
         }
-        const token = jwt.sign({username:username,role:user.role}, process.env.JWT_KEY);
+        const token = jwt.sign({ email: user.email, role: user.role }, process.env.JWT_KEY);
         res.send(token)
     } catch (error) {
         res.send(error.message)
@@ -21,9 +21,9 @@ export const loginController = async (req, res) => {
 }
 export const registerController = async (req, res) => {
     try {
-        const { password, email } = req.body
+        const { username, password, email } = req.body
         const newUser = new userModel(req.body)
-        const token = jwt.sign({username:username,role:"user"}, process.env.JWT_KEY);
+        const token = jwt.sign({ username: username, role: "user" }, process.env.JWT_KEY);
         await newUser.save()
         res.send(token)
     } catch (error) {
@@ -35,4 +35,3 @@ export const registerController = async (req, res) => {
 
 
 
-    ;
