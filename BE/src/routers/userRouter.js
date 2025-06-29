b2506783f313183131e0e27664dd2a70d881531c
Ilaha <ilaharf-bp218@code.edu.az>
2025-06-29 19:24:17 +0400

    Added validation for user

--- BE/src/routers/userRouter.js
+++ BE/src/routers/userRouter.js
@@ -1,11 +1,12 @@
 import { Router } from "express"
 import { createUser, deleteByIdUser, getAllUser, getByIdUser, updateByIdUser } from "../controllers/userController.js"
 import { verifyAccess } from "../middleware/authMiddleware.js"
-
+import { validate } from "../middleware/authMiddleware.js";
+import { userValidationSchema } from "../validations/userValidation.js";
 export const userRouter = Router()
 
 userRouter.get('/',verifyAccess(["admin"]), getAllUser)
     .get('/:id',verifyAccess(["admin","user"]), getByIdUser)
-    .post('/',verifyAccess(["admin"]), createUser)
-    .put('/:id',verifyAccess(["admin"]), updateByIdUser)
+    .post('/',verifyAccess(["admin"]),validate(userValidationSchema), createUser)
+    .put('/:id',verifyAccess(["admin"]),validate(userValidationSchema), updateByIdUser)
     .delete('/:id',verifyAccess(["admin"]), deleteByIdUser)
