fb5ad45ab0b230ad8cfe2c87bdc7d54382c5fe0a
Ilaha <ilaharf-bp218@code.edu.az>
2025-06-30 12:17:02 +0400

    Set up router system

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,33 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route } from "react-router";
+import MainRoot from './MainRoot'
+import Admin from './Pages/Admin/Admin'
+import AdminAdd from './Pages/AdminAdd/AdminAdd'
+import Detail from './Pages/Detail/Detail'
+import Home from './Pages/Home/Home'
+import NoPage from './Pages/NoPage/NoPage'
+import Wishlist from './Pages/Wishlist/Wishlist'
+import Profil from "./Pages/Profil/Profil";
+import Registration from "./Pages/Registration/Registration";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainRoot />}>
+            <Route index element={<Home />} />
+            <Route path="detail/:id" element={<Detail />} />
+            <Route path="wishlist" element={<Wishlist />} />
+            <Route path="admin" element={<Admin />} />
+            <Route path="adminadd" element={<AdminAdd />} />
+            <Route path="profil" element={<Profil />} />
+            <Route path="registration" element={<Registration />} />
+          </Route>
+          <Route path="*" element={<NoPage />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
