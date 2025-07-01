440b499eb0ef187ffa7009a7da8344d43f12182a
Ilaha <ilaharf-bp218@code.edu.az>
2025-07-02 00:17:35 +0400

    Footer section implemented

--- FE/src/Components/Footer/Footer.jsx
+++ FE/src/Components/Footer/Footer.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
-
+import { FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa";
+import '../Footer/Footer.css'
 function Footer() {
   return (
-    <div>Footer</div>
+    <>
+   <div className='footer_section'>
+    <h2>
+        Bizimlə əlaqə 
+    </h2>
+    <div className="footer_icons">
+       <div className="phone_number"> Əlaqə nömrəsi:+994551234564 </div>
+       <a href=""><FaInstagram /></a>
+       <a href=""><FaTwitter /></a>
+       <a href=""><FaFacebookF /></a>
+    </div>
+   </div>
+    </>
   )
 }
 
