cb2f5a57cf77098f6caea7c6bca95a77c7dc5bf9
Ilaha <ilaharf-bp218@code.edu.az>
2025-07-02 11:12:42 +0400

    Created NoPage

--- FE/src/Pages/NoPage/NoPage.jsx
+++ FE/src/Pages/NoPage/NoPage.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import { motion } from 'framer-motion';
+import { Link } from 'react-router';
+import '../NoPage/NoPage.css'
 
 function NoPage() {
   return (
-    <div>NoPage</div>
+    <>
+    <title>Error</title>
+    <div className="error-container"><motion.h1
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.8 }}
+      >
+        404
+      </motion.h1>
+      <motion.p
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ delay: 0.5 }}
+      >
+        Üzr istəyirik, səhifə tapılmadı.
+      </motion.p>
+      <motion.div className="button-link"
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.9 }}
+      >
+        <Link to="/">Ana səhifəyə qayıt</Link>
+      </motion.div></div>
+    </>
   )
 }
 
