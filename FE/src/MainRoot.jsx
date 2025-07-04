2de8a681cafeaa4e89db0b77fdb2a4b79d69578e
Ilaha <ilaharf-bp218@code.edu.az>
2025-07-04 22:18:14 +0400

    Implemented responsive design for Detail page

--- FE/src/MainRoot.jsx
+++ FE/src/MainRoot.jsx
@@ -1,16 +1,18 @@
-import { Outlet } from "react-router"
-import Navbar from "./Components/Navbar/Navbar"
-import Footer from "./Components/Footer/Footer"
-
+import { Outlet } from "react-router";
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/Footer/Footer";
+import "./MainRoot.css"; // bunu da əlavə et, aşağıdakı CSS-ə görə
 
 function MainRoot() {
-    return (
-        <>
-            <Navbar />
-            <Outlet />
-            <Footer />
-        </>
-    )
+  return (
+    <div className="page-container">
+      <Navbar />
+      <main className="content">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
-export default MainRoot
+export default MainRoot;
